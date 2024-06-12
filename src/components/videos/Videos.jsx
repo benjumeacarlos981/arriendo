40949cd8177806008634358a8becc2e9aa8cef51
Hector Javier Moreno <benjumeacarlos981@gmail.com>
2024-06-12 18:08:59 -0500

    deployando Codigo

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './videos.scss';
 
-function Videos({ data }) {
+function Videos({data}) {
+
+  useEffect(() => {
+    // Esto se asegura de que el script de Vimeo se cargue cuando el componente se monte
+    const script = document.createElement('script');
+    script.src = "https://player.vimeo.com/api/player.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Limpieza del script cuando el componente se desmonte
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
 
   return (
-      <div className="videos">
-        {data.videos.map((video, index) => (
-          <video key={index} controls>
-            <source src={video} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        ))}
-      </div>
+    <>
+     <div className="videos">
+      {data.videos.map((videoSrc, index) => (
+        <div key={index} style={{ padding: '49.76% 0 0 0', position: 'relative', marginBottom: '20px' }}>
+          <iframe
+            src={videoSrc}
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+            style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}
+            title={`Video-${index}`}
+          ></iframe>
+        </div>
+      ))}
+    </div>
+
+    </>
   );
 }
 
 export default Videos;
+
+
+
