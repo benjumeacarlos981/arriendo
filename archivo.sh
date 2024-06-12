25a110262c26f50a123e9a434daa62d6a9309f50
Hector Javier Moreno <benjumeacarlos981@gmail.com>
2024-06-12 11:04:14 -0500

    deployando Codigo

---
+++ archivo.sh
@@ -0,0 +1,18 @@
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/benjumeacarlos
+
+# Deploy codigo en glosariodeterminos hectorjaviermorenoh
+git remote remove origin
+git remote add origin git@github.com:benjumeacarlos981/arriendo.git
+git status
+git add .
+git commit -m "deployando Codigo"
+git push origin main
+npm run deploy
+
+
+
+
+
+
+
