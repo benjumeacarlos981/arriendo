1b242eb9c933ccb7ee3ed8fce7b49a5bca46ac7a
Hector Javier Moreno <benjumeacarlos981@gmail.com>
2024-06-12 14:38:07 -0500

    deployando Codigo

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // base: 'https://benjumeacarlos981.github.com/gh-pages',
+  base: 'https://benjumeacarlos981.github.com/gh-pages',
   server: {
     port: 3000
   },
