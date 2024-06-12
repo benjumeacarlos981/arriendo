11c8bc7fd615cb379a73da6350c57d45f83f8413
Hector Javier Moreno <benjumeacarlos981@gmail.com>
2024-06-12 16:29:48 -0500

    deployando Codigo

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx'
 import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
 const contenedor = document.getElementById('root');
 
 ReactDOM.render(<App />, contenedor);
