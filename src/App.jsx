2159d40df6c382b6619b76ed53155d257819a391
Hector Javier Moreno <benjumeacarlos981@gmail.com>
2024-06-12 16:03:15 -0500

    deployando Codigo

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch('/data/data.json')
+    fetch('/data/datos.json')
       .then(response => response.json())
       .then(data => setData(data));
   }, []);
