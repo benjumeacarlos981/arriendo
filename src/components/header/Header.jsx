624d320a5a91cedf711aff1cd77b62ab2e2f14e6
Hector Javier Moreno <benjumeacarlos981@gmail.com>
2024-06-13 13:34:08 -0500

    deployando Codigo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,8 +6,14 @@ function Header({ data }) {
   return (
     <div>
       <header>
+        <div className="left">
         <h1>{data.titulo1}</h1>
         <h2>{data.titulo2}</h2>
+        </div>
+        <div className="right">
+          <h1>$ 1.700.000</h1>
+          <h4>Cel 3182438948</h4>
+        </div>
       </header>
     </div>
 
